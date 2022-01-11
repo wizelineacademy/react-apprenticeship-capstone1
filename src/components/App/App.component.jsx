64b02b0a00886f56fbfc05b25f69c83ed8bf6678
Alma Alvarez <alma1997alvarez@gmail.com>
2022-01-11 14:40:47 -0600

    feat: added various new components

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,57 +1,59 @@
-import React, { useLayoutEffect } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+//import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+import videos from '../../mockvideos/youtube-mock-videos.json';
 
-import AuthProvider from '../../providers/Auth';
-import HomePage from '../../pages/Home';
-import LoginPage from '../../pages/Login';
-import NotFound from '../../pages/NotFound';
-import SecretPage from '../../pages/Secret';
-import Private from '../Private';
-import Fortune from '../Fortune';
-import Layout from '../Layout';
-import { random } from '../../utils/fns';
+// import AuthProvider from '../../providers/Auth';
+// import HomePage from '../../pages/Home';
+// import LoginPage from '../../pages/Login';
+// import NotFound from '../../pages/NotFound';
+// import Private from '../Private';
+// import Fortune from '../Fortune';
+// import Layout from '../Layout';
+// import { random } from '../../utils/fns';
+import MainHeader from '../Header';
+import Heading from '../Heading';
+import MainContainer from '../MainContainer';
+import VideosContainer from '../VideosContainer';
+import VideoCard from '../VideoCard';
+
+// const Button = styled.button`
+//   background: transparent;
+//   border-radius: 3px;
+//   border: 2px solid palevioletred;
+//   color: palevioletred;
+//   margin: 0.5em 1em;
+//   padding: 0.25em 1em;
+
+//   ${props => props.primary && css`
+//     background: palevioletred;
+//     color: white;
+//   `}
+// `;
 
 function App() {
-  useLayoutEffect(() => {
-    const { body } = document;
-
-    function rotateBackground() {
-      const xPercent = random(100);
-      const yPercent = random(100);
-      body.style.setProperty('--bg-position', `${xPercent}% ${yPercent}%`);
-    }
-
-    const intervalId = setInterval(rotateBackground, 3000);
-    body.addEventListener('click', rotateBackground);
-
-    return () => {
-      clearInterval(intervalId);
-      body.removeEventListener('click', rotateBackground);
-    };
-  }, []);
+  const videosArray = videos.items;
+  console.log(videosArray[0].id);
 
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Layout>
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route exact path="/login">
-              <LoginPage />
-            </Route>
-            <Private exact path="/secret">
-              <SecretPage />
-            </Private>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-          <Fortune />
-        </Layout>
-      </AuthProvider>
-    </BrowserRouter>
+    <React.Fragment>
+      <MainHeader></MainHeader>
+      <MainContainer>
+        <Heading title="Welcome to the challenge!"></Heading>
+        <VideosContainer>
+          {videosArray.map((video) => {
+            return (
+              <VideoCard
+                imgsrc={video.snippet.thumbnails.medium.url}
+                title={video.snippet.title}
+                description={video.snippet.description}
+                key={video.id.videoId}
+              />
+            );
+          })}
+        </VideosContainer>
+      </MainContainer>
+    </React.Fragment>
   );
 }
 
