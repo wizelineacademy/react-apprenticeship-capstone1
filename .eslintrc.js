c1e7e67651a402e71bdc59fa0e8bc508e68f0432
Maximiliano Hernandez Castillo <maximiliano.hernande@MaximilianosMBP.huawei.net>
2022-01-12 16:28:47 -0600

    test: added first tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     'env': {
         'browser': true,
-        'es6': true
+        'es6': true,
+        'jest/globals': true
     },
     'extends': [
         'eslint:recommended',
@@ -20,7 +21,8 @@ module.exports = {
         'sourceType': 'module'
     },
     'plugins': [
-        'react'
+        'react',
+        'jest'
     ],
     'rules': {
         'react/react-in-jsx-scope': 'off', 
--- src/components/IconButton/__test__/IconButton.test.js
+++ src/components/IconButton/__test__/IconButton.test.js
@@ -3,32 +3,26 @@ import IconButton from '../IconButton.component.jsx';
 
 const mockedClickableFunction = jest.fn();
 
-describe("Testing IconButton...", () => {
-    it('should render', () => {
-        render(
-            <IconButton icon={<i>icon</i>} />
-        );
+describe('Testing IconButton...', () => {
+  it('should render', () => {
+    render(<IconButton icon={<i>icon</i>} />);
 
-        let element = screen.getByTestId('icon-button');
-        expect(element).toBeInTheDocument();
-    });
+    let element = screen.getByTestId('icon-button');
+    expect(element).toBeInTheDocument();
+  });
 
-    it('should react when is clicked', () => {
-        render(
-            <IconButton icon={<i>icon</i>} onClick={mockedClickableFunction} />
-        );
+  it('should react when is clicked', () => {
+    render(<IconButton icon={<i>icon</i>} onClick={mockedClickableFunction} />);
 
-        let element = screen.getByTestId('icon-button');
-        fireEvent.click(element);
-        expect(mockedClickableFunction).toBeCalled();
-    });
+    let element = screen.getByTestId('icon-button');
+    fireEvent.click(element);
+    expect(mockedClickableFunction).toBeCalled();
+  });
 
-    it('should be visible', () => {
-        render(
-            <IconButton icon={<i>icon</i>} />
-        );
-    
-        let element = screen.getByTestId('icon-button');
-        expect(element).toBeVisible();
-    });
+  it('should be visible', () => {
+    render(<IconButton icon={<i>icon</i>} />);
+
+    let element = screen.getByTestId('icon-button');
+    expect(element).toBeVisible();
+  });
 });
