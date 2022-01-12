e70e6a73ed265205fed7bd114d721950a387970d
marcelavbc <marcela.vilasboas@gmail.com>
2022-01-12 17:18:32 +0100

    build: add tests to each component

--- src/components/Container/Container.component.jsx
+++ src/components/Container/Container.component.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledContainer } from './Container.styled';
 
 import Card from '../Card/Card.component';
 import data from '../../data.json';
+import axios from 'axios';
 
 function Container() {
+  const [videos, setVideos] = useState([]);
+  const [error, setError] = useState(null)
+  videos = response.data
+
+  useEffect(() => {
+    axios
+    .get('url')
+    .then(resp => {
+      setVideos(resp.data)
+    })
+    .catch(error => setError(error))
+    
+    })
+    return () => {};
+  }, []);
+
   return (
     <StyledContainer>
       {data.items.map((video, index) => (
