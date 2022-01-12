bc6aa4e5948e6c3d6519a22b62ab3c58f7d58bc0
Maximiliano Hernandez Castillo <maximiliano.hernande@MaximilianosMBP.huawei.net>
2022-01-12 16:19:05 -0600

    test: added first tests

---
+++ src/components/IconButton/__test__/IconButton.test.js
@@ -0,0 +1,34 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import IconButton from '../IconButton.component.jsx';
+
+const mockedClickableFunction = jest.fn();
+
+describe("Testing IconButton...", () => {
+    it('should render', () => {
+        render(
+            <IconButton icon={<i>icon</i>} />
+        );
+
+        let element = screen.getByTestId('icon-button');
+        expect(element).toBeInTheDocument();
+    });
+
+    it('should react when is clicked', () => {
+        render(
+            <IconButton icon={<i>icon</i>} onClick={mockedClickableFunction} />
+        );
+
+        let element = screen.getByTestId('icon-button');
+        fireEvent.click(element);
+        expect(mockedClickableFunction).toBeCalled();
+    });
+
+    it('should be visible', () => {
+        render(
+            <IconButton icon={<i>icon</i>} />
+        );
+    
+        let element = screen.getByTestId('icon-button');
+        expect(element).toBeVisible();
+    });
+});
