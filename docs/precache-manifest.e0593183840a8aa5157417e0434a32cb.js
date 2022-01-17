2aa0304e660f7e39bcb49c695bbe97ba773a0356
edv4ldez <edwin.valdez@wizeline.com>
2022-01-16 22:34:41 -0600

    capston3

---
+++ docs/precache-manifest.e0593183840a8aa5157417e0434a32cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6085bee4edaf19bbba63237c19a2c82d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ded1c3d09c1ec9330b69",
+    "url": "/static/css/main.30a11902.chunk.css"
+  },
+  {
+    "revision": "0f3f80d3cd307d1b4dde",
+    "url": "/static/js/2.4f1ad46a.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.4f1ad46a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded1c3d09c1ec9330b69",
+    "url": "/static/js/main.4f0ed9ce.chunk.js"
+  },
+  {
+    "revision": "0d4e3fbca24db45e5036",
+    "url": "/static/js/runtime-main.86719c39.js"
+  }
+]);
