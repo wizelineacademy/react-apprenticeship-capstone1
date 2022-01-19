dd91da5855cf915b0a5ffec16b1a70a305e900eb
sueherrera30 <sueherrera30@gmail.com>
2022-01-19 15:27:20 -0600

    feat: build

---
+++ build/precache-manifest.346fe6cb60eb870e1f17eeff07e489d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3ab4ec88bd915534449b2ad5a0bb47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e88b593614529fa64ba4",
+    "url": "/static/css/2.da8127bb.chunk.css"
+  },
+  {
+    "revision": "5f5bbe26c17f0a1e4a13",
+    "url": "/static/css/main.6b3b1198.chunk.css"
+  },
+  {
+    "revision": "e88b593614529fa64ba4",
+    "url": "/static/js/2.d913348f.chunk.js"
+  },
+  {
+    "revision": "fcedde40e9560a6d9cf48be932246e9b",
+    "url": "/static/js/2.d913348f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5bbe26c17f0a1e4a13",
+    "url": "/static/js/main.bb1a0ac0.chunk.js"
+  },
+  {
+    "revision": "0d4e3fbca24db45e5036",
+    "url": "/static/js/runtime-main.86719c39.js"
+  }
+]);
